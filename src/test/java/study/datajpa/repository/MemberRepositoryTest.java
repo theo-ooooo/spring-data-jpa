abe6f9234b471d685c09a5632ea23e106bb623a2
theo-ooooo <kkw.theo@gmail.com>
2025-04-22 13:49:28 +0900

    feat: 공통 인터페이스 적용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,11 +1,14 @@
 package study.datajpa.repository;
 
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +29,34 @@ class MemberRepositoryTest {
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        Assertions.assertEquals(findMember1, member1);
+        Assertions.assertEquals(findMember2, member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertEquals(2, all.size());
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        Assertions.assertEquals(2, count);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deleteCount = memberRepository.count();
+        Assertions.assertEquals(0, deleteCount);
     }
 }
