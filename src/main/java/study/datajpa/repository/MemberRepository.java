2d15bbe3f8640ed3f6f163b1824d7f71ff2e6c4b
theo-ooooo <kkw.theo@gmail.com>
2025-04-22 17:58:20 +0900

    feat: 사용자 정의 리포지토리 구현

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,68 +1,9 @@
 package study.datajpa.repository;
 
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.QueryHint;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
-import study.datajpa.dto.MemberDto;
+
 import study.datajpa.entity.Member;
 
-import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
-
-//    @Query(name = "Member.findByUsername")
-//    List<Member> findByUsername(@Param("username") String username);
-
-    @Query("select m from Member m where m.username = :username and m.age = :age")
-    List<Member> findUser(@Param("username") String username, @Param("age") int age);
-
-    @Query("select m.username from Member m")
-    List<String> findUsernameList();
-
-    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
-            "from Member m join m.team t")
-    List<MemberDto> findMemberDto();
-
-    @Query("select m from Member m where m.username = :name")
-    Member findMembers(@Param("name") String name);
-
-    @Query("select m from Member m where m.username in :names")
-    List<Member> findByNames(@Param("names") List<String> names);
-
-
-    @Query(value = "select m from Member m")
-    Page<Member> findByAge(int age, Pageable pageable);
-
-    @Modifying
-    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
-    int bulkAgePlus(@Param("age") int age);
-
-    @Query("select m from Member m left join fetch m.team")
-    List<Member> findByMemberFetchJoin();
-
-
-    // 공통 메서드
-    @Override
-    @EntityGraph(attributePaths = {"team"})
-    List<Member> findAll();
-
-    //JPQL + 엔티티 그래프
-    @EntityGraph(attributePaths = {"team"})
-    @Query("select m from Member m")
-    List<Member> findByMemberEntityGraph();
-
-    //메서드 이름으로 쿼리에서 특히 편리하다
-    @EntityGraph(attributePaths = {"team"})
-    List<Member> findByUsername(String username);
-
-    @QueryHints(value = @QueryHint(name ="org.hibernate.readOnly", value = "true"))
-    Member findReadOnlyByUsername(String name);
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    List<Member> findLockedByUsername(String username);
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 }
