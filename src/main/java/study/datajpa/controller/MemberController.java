d23229fea3c2ca161664db357798fbbc523112cb
theo-ooooo <kkw.theo@gmail.com>
2025-04-22 23:03:16 +0900

    feat: Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,9 +2,11 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -19,8 +21,21 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping(("/members"))
+    public Page<MemberDto> list(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::from);
+    }
+
+    @RequestMapping(value = "/members-page", method = RequestMethod.GET)
+    public String listPage(@PageableDefault(size = 12) Pageable pageable) {
+        return "kk";
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+
     }
 }
--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -9,6 +9,7 @@ public class MemberDto {
     private String username;
     private String teamName;
 
+
     public MemberDto(Long id, String username, String teamName) {
         this.id = id;
         this.username = username;
